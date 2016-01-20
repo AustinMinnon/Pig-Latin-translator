91d7bd8becbd4a27b970c39e5780eea5f828f5ec
Austin Minnon and Joseph Karasek <whi7ee@gmail.com>
2016-01-20 09:13:29 -0800

    add function to catch a period to findWord

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var findWord = function(inputString, inputIndex) {
   // loop that starts at the first letter, and continues until it finds a space
   var spaceIndex = inputIndex;
-  for(var i=inputIndex; inputString[i]!==" "; i++ ) {
+  for(var i=inputIndex; inputString[i]!==" " && inputString[i]!=="."; i++ ) {
     spaceIndex ++;
   }
   // return a slice of the input from 0 to the first space
