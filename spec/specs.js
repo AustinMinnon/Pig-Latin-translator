7c73a1d0591fbe8e306d9810468f30c51ef1c2bc
Austin Minnon and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-20 09:06:08 -0800

    add spec for findWord, end of sentence, spec causes infinite loop

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('findWord', function() {
   it('return a string of characters starting somewhere other than the beginning of the sentence', function() {
     expect(findWord("first word in the sentence", 6)).to.equal("word");
   });
+  it('return the last word of a sentence, that ends with a period', function() {
+    expect(findWord("first word in the sentence.", 18)).to.equal("sentence");
+  });
 });
